cbf495fe200b2e66ce713b840cb9bd9c706a211e
Holger Steinhauer <holger@steinhauer.software>
2021-01-13 21:12:13 +0100

    Adds Gatling
    This adds a simple Gatling subproject. Run it with `gradle :performance:gatlingRun`.
    So far, the endpoint and basic auth details are not configurable and it doesn't run standalone.

---
+++ performance/src/gatling/scala/org/fosdem/steinhauer/demo/performance/FosdemSimulation.scala
@@ -0,0 +1,24 @@
+package org.fosdem.steinhauer.demo.performance
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+
+
+class FosdemSimulation extends Simulation {
+
+  val httpProtocol = http.baseUrl("http://localhost:8080")
+    .acceptHeader("application/json")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Fosdem 2021 - This Spring Shall Be Challenged - Gatling Performance Tester")
+    .basicAuth("fosdem", "Fosdem.2021!")
+
+  val scn = scenario("Engage")
+    .exec(http("get all articles").get("/article/"))
+
+
+  setUp(scn.inject(rampUsers(1000) during (2 minutes)).protocols(httpProtocol)).maxDuration(5 minutes)
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "this-spring-shall-be-challenged"
-include("shared", "spring", "ktor", "micronaut", "jooby")
+include("shared", "spring", "ktor", "micronaut", "jooby", "performance")
